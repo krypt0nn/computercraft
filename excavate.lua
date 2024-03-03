facf7de4850eb6d02d8e901a30aaaceca8f1d98a
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2024-03-03 15:25:53 +0200

    build(excavate): bump excavation lib version

--- excavate.lua
+++ excavate.lua
@@ -3,7 +3,7 @@ local packages = dofile("require.lua")({
     cache = "cache",
     packages = {
         excavation = {
-            minimalVersion = 3
+            minimalVersion = 4
         }
     }
 })
