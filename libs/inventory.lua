c8936c194792cf9ff114b2ee638368a3e6edd99e
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2024-05-11 17:15:02 +0200

    feat(lib/inventory): added `listItems` method

--- libs/inventory.lua
+++ libs/inventory.lua
@@ -1,22 +1,22 @@
 local function info()
     return {
-        version = 2
+        version = 3
     }
 end
 
 -- Check if given peripheral name is an inventory
-local function isInventory(side, name)
+local function isInventory(name)
     local pullItems = false
     local pushItems = false
     local list = false
 
-    local methods = peripheral.wrap(side).getMethodsRemote(name)
+    local inventory = peripheral.wrap(name)
 
-    if not methods then
+    if not inventory then
         return false
     end
 
-    for _, name in pairs(methods) do
+    for name, _ in pairs(inventory) do
         if name == "pullItems" then
             pullItems = true
         elseif name == "pushItems" then
@@ -29,7 +29,31 @@ local function isInventory(side, name)
     return pullItems and pushItems and list
 end
 
+-- Get table of items in given inventory
+local function listItems(name)
+    if not isInventory(name) then
+        return nil
+    end
+
+    local itemsRaw = peripheral.wrap(name).list()
+    local items = {}
+
+    for _, item in pairs(itemsRaw) do
+        local value = items[item.name] or {
+            name  = item.name,
+            count = 0
+        }
+
+        value.count = value.count + item.count
+
+        items[item.name] = value
+    end
+
+    return items
+end
+
 return {
     info = info,
-    isInventory = isInventory
+    isInventory = isInventory,
+    listItems = listItems
 }
