f045ced87b8415e7110ed113c8ef35ce953422a6
Nikita Podvirnyi <krypt0nn@vk.com>
2024-05-17 23:44:39 +0200

    feat(apps/craft): added initial TUI

--- apps/craft.lua
+++ apps/craft.lua
@@ -3,14 +3,38 @@ local packages = dofile("require.lua")({
     cache = "cache",
     packages = {
         inventory = {
-            minimalVersion = 2
+            minimalVersion = 15
+        },
+        recipes = {
+            minimalVersion = 9
         }
     }
 })
 
-local side = "left"
-local inventory = "minecraft:chest_2"
+local inventory = "extended_drawers:access_point_0"
 
-if not packages.inventory.isInventory(side, inventory) then
-    error("Wrong inventory")
+if not packages.inventory.isInventory(inventory) then
+    error("Wrong inventory name")
+end
+
+while true do
+    print("What should I craft?")
+
+    local name = io.read("*l")
+
+    print("How many?")
+
+    local count = io.read("*n")
+
+    local craftQueue = packages.recipes.findRecipeExecutionQueue(
+        packages.inventory.listItems(inventory),
+        name,
+        count
+    )
+
+    if craftQueue then
+        print("Found craft with " .. #craftQueue .. " steps")
+    else
+        print("Couldn't find possible craft")
+    end
 end
