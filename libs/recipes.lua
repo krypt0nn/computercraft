dc0023d452828c438bfb1112a72a361c6a23f8ff
Nikita Podvirnyi <krypt0nn@vk.com>
2024-05-17 22:16:42 +0200

    feat(lib/recipes): updated `findRecipeExecutionQueue`

--- libs/recipes.lua
+++ libs/recipes.lua
@@ -1,6 +1,6 @@
 local function info()
     return {
-        version = 5
+        version = 6
     }
 end
 
@@ -93,7 +93,10 @@ local function findRecipes(item)
     for _, recipe in pairs(recipes()) do
         for _, output in pairs(recipe.output) do
             if string.find(output.name, item) then
-                table.insert(foundRecipes, recipe)
+                table.insert(foundRecipes, {
+                    recipe = recipe,
+                    count = output.count
+                })
 
                 break
             end
@@ -105,52 +108,75 @@ end
 
 -- Try to find the most optimal recipe execution queue
 -- to craft an item from available resources
-local function findRecipeExecutionQueue(available, item) -- TODO: quantity (craft)
+local function findRecipeExecutionQueue(available, item, count)
+    -- Try to find known recipes for needed item
     for _, recipe in pairs(findRecipes(item)) do
         local remainingResources = available
 
-        local queue = {
-          recipe
-        }
+        local queue = {}
+        local correctQueue = true
 
-        local correctRecipe = true
+        -- Put crafts to the queue to get needed amount of items
+        for i = 0, count / recipe.count do
+            local correctRecipe = true
 
-        for _, input in pairs(recipe.input) do
-            local craft = 0
+            -- Go through recipe input resources
+            for _, input in pairs(recipe.input) do
+                local craft = 0
 
-            if not remainingResources[input.name] then
-                craft = input.count
-            elseif input.count > remainingResources[input.name].count then
-                craft = input.count - remainingResources[input.name].count
+                -- If none available - craft needed amount
+                if not remainingResources[input.name] then
+                    craft = input.count
 
-                remainingResources[input.name].count = 0
-            else
-                remainingResources[input.name].count = remainingResources[input.name].count - input.count
-            end
+                -- If more than available needed - craft what's absent
+                elseif input.count > remainingResources[input.name].count then
+                    craft = input.count - remainingResources[input.name].count
 
-            -- TODO: respect craft variable
-            if craft > 0 then
-                local inputCraftQueue = findRecipeExecutionQueue(remainingResources, input.name)
+                    remainingResources[input.name].count = 0
 
-                if not inputCraftQueue then
-                    correctRecipe = false
-
-                    break
+                -- Otherwise we have just enough resources so only need
+                -- to decreese their remaining value
+                else
+                    remainingResources[input.name].count = remainingResources[input.name].count - input.count
                 end
 
-                for _, oldQueue in pairs(queue) do
-                    table.insert(inputCraftQueue, oldQueue)
-                end
+                -- If we need to craft anything
+                if craft > 0 then
+                    -- Prepare needed resources crafting queue
+                    local inputCraftQueue = findRecipeExecutionQueue(remainingResources, input.name, craft)
 
-                queue = inputCraftQueue
+                    -- Stop search if it's not available
+                    if not inputCraftQueue then
+                        correctRecipe = false
+
+                        break
+                    end
+
+                    -- Otherwise merge the queues
+                    for _, inputCraftRecipe in pairs(inputCraftQueue) do
+                        table.insert(queue, inputCraftRecipe)
+                    end
+                end
             end
+
+            -- Stop search if the recipe is incorrect
+            if not correctRecipe then
+                correctQueue = false
+
+                break
+            end
+
+            -- Put the recipe to the queue otherwise
+            table.insert(queue, recipe.recipe)
         end
 
-        if correctRecipe then
+        -- Return prepared craft queue if everything is correct
+        if correctQueue then
             return queue
         end
     end
 
+    -- Recipe wasn't found
     return nil
 end
 
