c082fbe9823bec94f6985a510d69323a8053ae9b
Nikita Podvirnyi <krypt0nn@vk.com>
2024-05-18 10:08:51 +0200

    build(apps/turtle_craft): updated `crafter`

--- apps/turtle_craft.lua
+++ apps/turtle_craft.lua
@@ -3,7 +3,7 @@ local packages = dofile("require.lua")({
     cache = "cache",
     packages = {
         crafter = {
-            minimalVersion = 6
+            minimalVersion = 7
         }
     }
 })
