e7ca1e29c6b66020aed8a138fde834db81b33878
Nikita Podvirnyi <krypt0nn@vk.com>
2024-05-19 12:11:43 +0200

    feat(apps/craft): used `recipes_runtime`

--- apps/craft.lua
+++ apps/craft.lua
@@ -8,8 +8,8 @@ local packages = dofile("require.lua")({
         recipes = {
             minimalVersion = 27
         },
-        crafter = {
-            minimalVersion = 8
+        recipes_runtime = {
+            minimalVersion = 1
         }
     }
 })
@@ -19,30 +19,18 @@ local packages = dofile("require.lua")({
 -- Rednet modem interface
 local modem = "top"
 
--- Crafting turtle ID
-local crafterId = 1
-
--- Crafting turtle input inventory interface name
-local crafterInputInventory = "minecraft:barrel_1"
-
--- Crafting turtle output inventory interface name
-local crafterOutputInventory = "minecraft:barrel_2"
-
 -- Main storage interface name
 local storageInventory = "toms_storage:ts.inventory_connector_0"
 
 -- Perform recipes queues optimizations (experimental)
 local optimizeRecipes = true
 
+-- Register recipes executers pool
+local pool = packages.recipes_runtime.pool({
+    packages.recipes_runtime.crafter(1, "minecraft:barrel_1", "minecraft:barrel_2")
+})
+
 ------------------------------- Settings -------------------------------
-
-if not packages.inventory.isInventory(crafterInputInventory) then
-    error("Wrong crafter input inventory name")
-end
-
-if not packages.inventory.isInventory(crafterOutputInventory) then
-    error("Wrong crafter output inventory name")
-end
 
 if not packages.inventory.isInventory(storageInventory) then
     error("Wrong main storage inventory name")
@@ -84,87 +72,45 @@ while true do
         for step, recipe in pairs(craftQueue) do
             local prefix = "[" .. math.floor(step / #craftQueue * 100) .. "%] "
 
-            if recipe.action ~= "craft" then
-                print(prefix .. "Not a crafting action. Stopping execution")
+            -- Execute recipe
+            local result, reason = packages.recipes_runtime.executeRecipe(storageInventory, recipe, pool)
+
+            -- Stop execution if failed
+            if not result then
+                print(prefix .. "Couldn't execute recipe: " .. reason)
 
                 break
-            else
-                local continueCrafting = true
-
-                for _, input in pairs(recipe.params.recipe) do
-                    local moved = packages.inventory.moveItems(storageInventory, crafterInputInventory, input.name, input.count)
-
-                    if not moved or moved < input.count then
-                        print(prefix .. "Couldn't transfer enough resources. Stopping execution")
-
-                        continueCrafting = false
-
-                        break
-                    end
-                end
-
-                if not continueCrafting then
-                    break
-                end
-
-                -- Get output inventory state before requesting craft
-                local outputInventoryItems = packages.inventory.listItems(crafterOutputInventory)
-
-                -- Send crafting request to the turtle
-                if not packages.crafter.sendRecipe(tonumber(crafterId), recipe) then
-                    print(prefix .. "Couldn't request recipe crafting. Stopping execution")
-
-                    break
-                end
-
-                local craftedSuffix = ""
-
-                -- Go through the expected recipe outputs
-                for _, output in pairs(recipe.output) do
-                    -- Wait until the craft is finished
-                    while true do
-                        sleep(1)
-
-                        local foundItem = packages.inventory.findItem(crafterOutputInventory, output.name)
-
-                        -- If we have found the item and it either
-                        -- wasn't presented in the output inventory before crafting
-                        -- or its value is different now
-                        -- 
-                        -- This is needed because we can't move some craft results to the
-                        -- output (global storage) inventory
-                        if foundItem ~= nil and (not outputInventoryItems[foundItem.name] or foundItem.count > outputInventoryItems[foundItem.name].count) then
-                            break
-                        end
-                    end
-
-                    -- Move it to the storage
-                    packages.inventory.moveItems(crafterOutputInventory, storageInventory, output.name)
-
-                    -- Append suffix
-                    if craftedSuffix == "" then
-                        craftedSuffix = "[" .. output.name .. "] x" .. output.count
-                    else
-                        craftedSuffix = craftedSuffix .. ", [" .. output.name .. "] x" .. output.count
-                    end
-                end
-
-                local craftingTime = os.epoch("utc") - craftStartTime
-                local craftingEta = math.ceil((#craftQueue * craftingTime / step - craftingTime) / 10) / 100
-
-                print(prefix .. "Crafted " .. craftedSuffix .. ". ETA: " .. craftingEta .. " sec")
             end
-        end
 
-        -- Move crafted thing to the storage
-        packages.inventory.moveItems(crafterOutputInventory, storageInventory, name)
+            -- Add recipe execution time to the prefix
+            local recipeTime = math.ceil(result.time.duration / 10) / 100
+
+            prefix = prefix .. "[" .. recipeTime .. " sec] "
+
+            -- List recipe result
+            local recipeResult = ""
+
+            for _, result in pairs(result.result) do
+                if recipeResult == "" then
+                    recipeResult = "[" .. result.name "] x" .. result.count
+                else
+                    recipeResult = recipeResult .. ", [" .. result.name "] x" .. result.count
+                end
+            end
+
+            -- Calculate total ETA
+            local craftingTime = os.epoch("utc") - craftStartTime
+            local craftingEta = math.ceil((#craftQueue * craftingTime / step - craftingTime) / 10) / 100
+
+            print(prefix .. "Crafted " .. recipeResult .. ". ETA: " .. craftingEta .. " sec")
+        end
 
         -- Print crafting time
         local craftingTime = math.ceil((os.epoch("utc") - craftStartTime) / 10) / 100
 
         print("Craft finished in " .. craftingTime .. " sec")
     else
-        print("Couldn't find possible craft")
+        print("Couldn't find crafting queue")
 
         if #resourcesHint > 0 then
             print("Possible solutions:")
