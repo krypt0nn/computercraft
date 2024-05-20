9d51e67287b717d108e3dfe874986cb2cd73d82d
Nikita Podvirnyi <krypt0nn@vk.com>
2024-05-20 13:11:43 +0200

    build(apps/craft): updated `recipes` version

--- apps/craft.lua
+++ apps/craft.lua
@@ -6,7 +6,7 @@ local packages = dofile("require.lua")({
             minimalVersion = 18
         },
         recipes = {
-            minimalVersion = 47
+            minimalVersion = 48
         },
         recipes_runtime = {
             minimalVersion = 7
