8ecb78b13804a7b3727fcfe4a55b24334ba60a48
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-21 01:41:23 +0200

    feat: add uncooked steel dust recipe

---
+++ apps/autocraft_v2/recipes/disk_2/coke_dust.lua
@@ -0,0 +1,9 @@
+return {
+    machine = "macerator",
+    inputs = {
+        { name = "modern_industrialization:coke", count = 1 }
+    },
+    outputs = {
+        { name  = "modern_industrialization:coke_dust", count = 1 }
+    }
+}
---
+++ apps/autocraft_v2/recipes/disk_2/uncooked_steel_dust.lua
@@ -0,0 +1,10 @@
+return {
+    machine = "mixer",
+    inputs = {
+        { name = "modern_industrialization:iron_dust", count = 7 },
+        { name = "modern_industrialization:coke_dust", count = 2 }
+    },
+    outputs = {
+        { name  = "modern_industrialization:uncooked_steel_dust", count = 9 }
+    }
+}
