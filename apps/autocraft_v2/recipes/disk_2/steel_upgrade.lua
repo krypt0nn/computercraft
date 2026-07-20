4d7e05ab7cc9185665d8738c68f5b12ff713c7a7
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-20 22:59:57 +0200

    feat: add steel upgrade

---
+++ apps/autocraft_v2/recipes/disk_2/steel_upgrade.lua
@@ -0,0 +1,17 @@
+return {
+    machine = "crafter",
+    inputs = {
+        { name = "modern_industrialization:bronze_rod",           count = 1 },
+        { name = "modern_industrialization:bronze_rotor",         count = 1 },
+        { name = "modern_industrialization:bronze_rod",           count = 1 },
+        { name = "modern_industrialization:bronze_gear",          count = 1 },
+        { name = "modern_industrialization:steel_machine_casing", count = 1 },
+        { name = "modern_industrialization:bronze_gear",          count = 1 },
+        { name = "modern_industrialization:fluid_pipe",           count = 1 },
+        { name = "modern_industrialization:fire_clay_bricks",     count = 1 },
+        { name = "modern_industrialization:fluid_pipe",           count = 1 }
+    },
+    outputs = {
+        { name  = "modern_industrialization:steel_upgrade", count = 1 }
+    }
+}
