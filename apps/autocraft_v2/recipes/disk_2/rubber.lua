c7b3f741e40e9898b2391135df18806c41b53eff
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-20 22:27:09 +0200

    feat: add rubber

---
+++ apps/autocraft_v2/recipes/disk_2/rubber.lua
@@ -0,0 +1,9 @@
+return {
+    machine = "furnace",
+    inputs = {
+        { name = "techreborn:sap", count = 1 }
+    },
+    outputs = {
+        { name  = "techreborn:rubber", count = 1 }
+    }
+}
---
+++ apps/autocraft_v2/recipes/disk_2/rubber_sheet.lua
@@ -0,0 +1,9 @@
+return {
+    machine = "furnace",
+    inputs = {
+        { name = "techreborn:rubber", count = 1 }
+    },
+    outputs = {
+        { name  = "modern_industrialization:rubber_sheet", count = 2 }
+    }
+}
