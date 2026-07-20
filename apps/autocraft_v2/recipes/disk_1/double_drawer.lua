d543f2a06e0498f969fd27bf398f24f2c4eedb48
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-20 23:08:49 +0200

    feat: add drawers

---
+++ apps/autocraft_v2/recipes/disk_1/double_drawer.lua
@@ -0,0 +1,17 @@
+return {
+    machine = "crafter",
+    inputs = {
+        { name = "minecraft:spruce_log",    count = 1 },
+        { name = "minecraft:spruce_planks", count = 1 },
+        { name = "minecraft:spruce_log",    count = 1 },
+        { name = "minecraft:chest",         count = 1 },
+        { name = "minecraft:spruce_planks", count = 1 },
+        { name = "minecraft:chest",         count = 1 },
+        { name = "minecraft:spruce_log",    count = 1 },
+        { name = "minecraft:spruce_planks", count = 1 },
+        { name = "minecraft:spruce_log",    count = 1 }
+    },
+    outputs = {
+        { name  = "extended_drawers:double_drawer", count = 1 }
+    }
+}
---
+++ apps/autocraft_v2/recipes/disk_1/drawer_access_point.lua
@@ -0,0 +1,17 @@
+return {
+    machine = "crafter",
+    inputs = {
+        { name = "minecraft:cobblestone",          count = 1 },
+        { name = "minecraft:iron_ingot",           count = 1 },
+        { name = "minecraft:cobblestone",          count = 1 },
+        { name = "minecraft:iron_ingot",           count = 1 },
+        { name = "extended_drawers:single_drawer", count = 1 },
+        { name = "minecraft:iron_ingot",           count = 1 },
+        { name = "minecraft:cobblestone",          count = 1 },
+        { name = "minecraft:iron_ingot",           count = 1 },
+        { name = "minecraft:cobblestone",          count = 1 }
+    },
+    outputs = {
+        { name  = "extended_drawers:access_point", count = 1 }
+    }
+}
