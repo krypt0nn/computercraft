9e0044166ce5d133da1960a6b5d5e8b36598c5f9
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-20 22:51:14 +0200

    feat: add copper and bronze rotors

---
+++ apps/autocraft_v2/recipes/disk_2/bronze_blade.lua
@@ -0,0 +1,13 @@
+return {
+    machine = "crafter",
+    inputs = {
+        { name = "modern_industrialization:bronze_curved_plate", count = 1 },
+        nil, nil,
+        { name = "modern_industrialization:bronze_curved_plate", count = 1 },
+        nil, nil,
+        { name = "modern_industrialization:bronze_rod",          count = 1 },
+    },
+    outputs = {
+        { name  = "modern_industrialization:bronze_blade", count = 4 }
+    }
+}
