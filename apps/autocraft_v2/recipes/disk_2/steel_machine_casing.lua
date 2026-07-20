ceb1159a0462b80e2712993aa0dc724fededcd22
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-20 22:56:36 +0200

    feat: add machine casings

---
+++ apps/autocraft_v2/recipes/disk_2/steel_machine_casing.lua
@@ -0,0 +1,17 @@
+return {
+    machine = "crafter",
+    inputs = {
+        { name = "modern_industrialization:steel_plate", count = 1 },
+        { name = "modern_industrialization:steel_plate", count = 1 },
+        { name = "modern_industrialization:steel_plate", count = 1 },
+        { name = "modern_industrialization:steel_plate", count = 1 },
+        { name = "modern_industrialization:steel_gear",  count = 1 },
+        { name = "modern_industrialization:steel_plate", count = 1 },
+        { name = "modern_industrialization:steel_plate", count = 1 },
+        { name = "modern_industrialization:steel_plate", count = 1 },
+        { name = "modern_industrialization:steel_plate", count = 1 }
+    },
+    outputs = {
+        { name  = "modern_industrialization:steel_machine_casing", count = 1 }
+    }
+}
---
+++ apps/autocraft_v2/recipes/disk_2/steel_machine_casing_pipe.lua
@@ -0,0 +1,17 @@
+return {
+    machine = "crafter",
+    inputs = {
+        { name = "modern_industrialization:steel_curved_plate",   count = 1 },
+        nil,
+        { name = "modern_industrialization:steel_curved_plate",   count = 1 },
+        { name = "modern_industrialization:steel_curved_plate",   count = 1 },
+        { name = "modern_industrialization:steel_machine_casing", count = 1 },
+        { name = "modern_industrialization:steel_curved_plate",   count = 1 },
+        { name = "modern_industrialization:steel_curved_plate",   count = 1 },
+        nil,
+        { name = "modern_industrialization:steel_curved_plate",   count = 1 }
+    },
+    outputs = {
+        { name  = "modern_industrialization:steel_machine_casing_pipe", count = 2 }
+    }
+}
