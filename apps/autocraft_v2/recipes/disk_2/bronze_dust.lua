bd2d7c8d2dde79cabd5345be06b7bd1214a11960
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-21 00:48:38 +0200

    feat: add bronze dust

---
+++ apps/autocraft_v2/recipes/disk_2/bronze_dust.lua
@@ -0,0 +1,10 @@
+return {
+    machine = "mixer",
+    inputs = {
+        { name = "modern_industrialization:tin_dust",    count = 1 },
+        { name = "modern_industrialization:copper_dust", count = 3 }
+    },
+    outputs = {
+        { name  = "modern_industrialization:bronze_ingot", count = 4 }
+    }
+}
