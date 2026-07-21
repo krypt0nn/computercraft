ba432fa76dc895b0606ab711120848c7700d7f9e
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-22 00:28:52 +0200

    feat: add more machines

---
+++ apps/autocraft_v2/recipes/steel_machines/steel_boiler.lua
@@ -0,0 +1,17 @@
+return {
+    machine = "crafter",
+    inputs = {
+        { name = "modern_industrialization:steel_plate",      count = 1 },
+        { name = "modern_industrialization:steel_tank",       count = 1 },
+        { name = "modern_industrialization:steel_plate",      count = 1 },
+        { name = "modern_industrialization:steel_plate",      count = 1 },
+        { name = "minecraft:furnace",                         count = 1 },
+        { name = "modern_industrialization:steel_plate",      count = 1 },
+        { name = "modern_industrialization:fire_clay_bricks", count = 1 },
+        { name = "modern_industrialization:fire_clay_bricks", count = 1 },
+        { name = "modern_industrialization:fire_clay_bricks", count = 1 }
+    },
+    outputs = {
+        { name = "modern_industrialization:steel_boiler", count = 1 }
+    }
+}
