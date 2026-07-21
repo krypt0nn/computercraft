4cf6f6122fc2dabce83e4ea1c11c0a9f005bc0a6
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-22 00:03:05 +0200

    feat: add electric cutting machine recipe

---
+++ apps/autocraft_v2/recipes/electric_machines/electric_cutting_machine.lua
@@ -0,0 +1,17 @@
+return {
+    machine = "crafter",
+    inputs = {
+        { name = "minecraft:glass",                             count = 1 },
+        { name = "modern_industrialization:invar_rotary_blade", count = 1 },
+        { name = "minecraft:glass",                             count = 1 },
+        { name = "modern_industrialization:motor",              count = 1 },
+        { name = "modern_industrialization:basic_machine_hull", count = 1 },
+        { name = "modern_industrialization:motor",              count = 1 },
+        { name = "modern_industrialization:analog_circuit",     count = 1 },
+        { name = "modern_industrialization:conveyor",           count = 1 },
+        { name = "modern_industrialization:analog_circuit",     count = 1 }
+    },
+    outputs = {
+        { name = "modern_industrialization:electric_cutting_machine", count = 1 }
+    }
+}
---
+++ apps/autocraft_v2/recipes/invar_parts/invar_rotary_blade.lua
@@ -0,0 +1,13 @@
+return {
+    machine = "crafter",
+    inputs = {
+        [2] = { name = "modern_industrialization:diamond_dust", count = 1 },
+        [4] = { name = "modern_industrialization:diamond_dust", count = 1 },
+        [5] = { name = "modern_industrialization:invar_gear",   count = 1 },
+        [6] = { name = "modern_industrialization:diamond_dust", count = 1 },
+        [8] = { name = "modern_industrialization:diamond_dust", count = 1 }
+    },
+    outputs = {
+        { name = "modern_industrialization:invar_rotary_blade", count = 1 }
+    }
+}
