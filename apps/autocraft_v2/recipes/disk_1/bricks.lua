7c9566627c99af5696a3e42c1b5bcd79d5a0bccb
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-21 17:53:12 +0200

    feat: add bricks recipe

---
+++ apps/autocraft_v2/recipes/disk_1/bricks.lua
@@ -0,0 +1,9 @@
+return {
+    machine = "compressor",
+    inputs = {
+        { name = "minecraft:brick", count = 4 }
+    },
+    outputs = {
+        { name = "minecraft:bricks", count = 1 }
+    }
+}
