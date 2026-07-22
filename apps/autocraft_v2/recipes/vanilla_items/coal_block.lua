b65896e2bb1f8b68379ff6ebde25287470311408
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-22 12:26:43 +0200

    feat: add diamond recipe

---
+++ apps/autocraft_v2/recipes/vanilla_items/coal_block.lua
@@ -0,0 +1,17 @@
+return {
+    machine = "furnace",
+    inputs = {
+        { name = "minecraft:coal", count = 1 },
+        { name = "minecraft:coal", count = 1 },
+        { name = "minecraft:coal", count = 1 },
+        { name = "minecraft:coal", count = 1 },
+        { name = "minecraft:coal", count = 1 },
+        { name = "minecraft:coal", count = 1 },
+        { name = "minecraft:coal", count = 1 },
+        { name = "minecraft:coal", count = 1 },
+        { name = "minecraft:coal", count = 1 }
+    },
+    outputs = {
+        { name = "minecraft:coal_block", count = 1 }
+    }
+}
---
+++ apps/autocraft_v2/recipes/vanilla_items/diamond.lua
@@ -0,0 +1,9 @@
+return {
+    machine = "compressor",
+    inputs = {
+        { name = "minecraft:coal_block", count = 16 }
+    },
+    outputs = {
+        { name = "minecraft:diamond", count = 1 }
+    }
+}
