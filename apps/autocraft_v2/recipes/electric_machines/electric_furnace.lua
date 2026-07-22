dee39c213c9c7f10cef641e914173c419f222b0f
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-22 13:54:08 +0200

    feat: add electric furnace recipe

---
+++ apps/autocraft_v2/recipes/electric_machines/electric_furnace.lua
@@ -0,0 +1,17 @@
+return {
+    machine = "crafter",
+    inputs = {
+        { name = "modern_industrialization:cupronickel_wire_magnetic", count = 1 },
+        { name = "modern_industrialization:analog_circuit",            count = 1 },
+        { name = "modern_industrialization:cupronickel_wire_magnetic", count = 1 },
+        { name = "modern_industrialization:cupronickel_wire_magnetic", count = 1 },
+        { name = "modern_industrialization:basic_machine_hull",        count = 1 },
+        { name = "modern_industrialization:cupronickel_wire_magnetic", count = 1 },
+        { name = "modern_industrialization:tin_cable",                 count = 1 },
+        { name = "modern_industrialization:analog_circuit",            count = 1 },
+        { name = "modern_industrialization:tin_cable",                 count = 1 }
+    },
+    outputs = {
+        { name = "modern_industrialization:electric_furnace", count = 1 }
+    }
+}
