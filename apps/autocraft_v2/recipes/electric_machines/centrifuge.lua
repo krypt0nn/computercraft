e73ecf2dec9b5628d5963c2454002fc4eaf9d9de
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-22 16:47:28 +0200

    feat: add centrifuge and electrolyzer recipes

---
+++ apps/autocraft_v2/recipes/electric_machines/centrifuge.lua
@@ -0,0 +1,17 @@
+return {
+    machine = "crafter",
+    inputs = {
+        { name = "modern_industrialization:large_motor",        count = 1 },
+        { name = "modern_industrialization:large_pump",         count = 1 },
+        { name = "modern_industrialization:large_motor",        count = 1 },
+        { name = "modern_industrialization:electrum_cable",     count = 1 },
+        { name = "modern_industrialization:basic_machine_hull", count = 1 },
+        { name = "modern_industrialization:electrum_cable",     count = 1 },
+        { name = "modern_industrialization:large_motor",        count = 1 },
+        { name = "modern_industrialization:electronic_circuit", count = 1 },
+        { name = "modern_industrialization:large_motor",        count = 1 }
+    },
+    outputs = {
+        { name = "modern_industrialization:centrifuge", count = 1 }
+    }
+}
---
+++ apps/autocraft_v2/recipes/electric_machines/electrolyzer.lua
@@ -0,0 +1,17 @@
+return {
+    machine = "crafter",
+    inputs = {
+        { name = "modern_industrialization:cupronickel_wire_magnetic", count = 1 },
+        { name = "modern_industrialization:electronic_circuit",        count = 1 },
+        { name = "modern_industrialization:cupronickel_wire_magnetic", count = 1 },
+        { name = "modern_industrialization:aluminum_rod",              count = 1 },
+        { name = "modern_industrialization:basic_machine_hull",        count = 1 },
+        { name = "modern_industrialization:aluminum_rod",              count = 1 },
+        { name = "modern_industrialization:cupronickel_wire_magnetic", count = 1 },
+        { name = "modern_industrialization:large_pump",                count = 1 },
+        { name = "modern_industrialization:cupronickel_wire_magnetic", count = 1 }
+    },
+    outputs = {
+        { name = "modern_industrialization:electrolyzer", count = 1 }
+    }
+}
