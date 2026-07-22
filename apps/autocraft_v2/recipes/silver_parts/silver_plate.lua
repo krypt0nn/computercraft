a2e5ed1ef5b21a3742ed5c77df270a77b9e58eab
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-22 16:51:00 +0200

    feat: add silver parts recipes

---
+++ apps/autocraft_v2/recipes/silver_parts/silver_plate.lua
@@ -0,0 +1,9 @@
+return {
+    machine = "compressor",
+    inputs = {
+        { name = "modern_industrialization:silver_ingot", count = 1 }
+    },
+    outputs = {
+        { name = "modern_industrialization:silver_plate", count = 1 }
+    }
+}
---
+++ apps/autocraft_v2/recipes/silver_parts/silver_wire.lua
@@ -0,0 +1,9 @@
+return {
+    machine = "wiremill",
+    inputs = {
+        { name = "modern_industrialization:silver_plate", count = 1 }
+    },
+    outputs = {
+        { name = "modern_industrialization:silver_wire", count = 2 }
+    }
+}
