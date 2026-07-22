23b542ffad77ab51ea9d3b8a1f2b91eb4fb61cfd
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-22 21:01:23 +0200

    feat: add steel ingot recipe

---
+++ apps/autocraft_v2/recipes/other/carbon_dust.lua
@@ -0,0 +1,9 @@
+return {
+    machine = "centrifuge",
+    inputs = {
+        { name = "modern_industrialization:lignite_coal_dust", count = 8 }
+    },
+    outputs = {
+        { name = "modern_industrialization:carbon_dust", count = 1 }
+    }
+}
---
+++ apps/autocraft_v2/recipes/steel_parts/steel_ingot.lua
@@ -0,0 +1,10 @@
+return {
+    machine = "electric_blast_furnace",
+    inputs = {
+        { name = "modern_industrialization:carbon_dust", count = 1 },
+        { name = "modern_industrialization:iron_dust",   count = 4 }
+    },
+    outputs = {
+        { name = "modern_industrialization:steel_ingot", count = 5 }
+    }
+}
