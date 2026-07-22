08f827ec3dff90151f572cf253c2e23d67f3008d
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-22 17:42:50 +0200

    feat: add bronze water pump recipe

---
+++ apps/autocraft_v2/recipes/bronze_machines/bronze_water_pump.lua
@@ -0,0 +1,17 @@
+return {
+    machine = "crafter",
+    inputs = {
+        { name = "modern_industrialization:copper_rotor",          count = 1 },
+        { name = "modern_industrialization:bronze_tank",           count = 1 },
+        { name = "modern_industrialization:copper_rotor",          count = 1 },
+        { name = "modern_industrialization:copper_gear",           count = 1 },
+        { name = "modern_industrialization:bronze_machine_casing", count = 1 },
+        { name = "modern_industrialization:copper_gear",           count = 1 },
+        { name = "modern_industrialization:fluid_pipe",            count = 1 },
+        { name = "modern_industrialization:fluid_pipe",            count = 1 },
+        { name = "modern_industrialization:fluid_pipe",            count = 1 }
+    },
+    outputs = {
+        { name = "modern_industrialization:bronze_water_pump", count = 1 }
+    }
+}
