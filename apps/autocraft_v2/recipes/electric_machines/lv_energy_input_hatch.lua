768c04574aad01d92b158150af3e630dddd24131
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-22 15:41:42 +0200

    feat: add lv energy hatches recipes

---
+++ apps/autocraft_v2/recipes/electric_machines/lv_energy_input_hatch.lua
@@ -0,0 +1,10 @@
+return {
+    machine = "crafter",
+    inputs = {
+        [1] = { name = "modern_industrialization:tin_table",          count = 1 },
+        [4] = { name = "modern_industrialization:basic_machine_hull", count = 1 }
+    },
+    outputs = {
+        { name = "modern_industrialization:lv_energy_input_hatch", count = 1 }
+    }
+}
