d297cd6eb7603617634d6b079047b6974244e320
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-22 18:18:35 +0200

    feat: add advanced machine casings recipes

---
+++ apps/autocraft_v2/recipes/aluminum_machines/advanced_machine_casing.lua
@@ -0,0 +1,17 @@
+return {
+    machine = "crafter",
+    inputs = {
+        { name = "modern_industrialization:aluminum_plate",     count = 1 },
+        { name = "modern_industrialization:aluminum_plate",     count = 1 },
+        { name = "modern_industrialization:aluminum_plate",     count = 1 },
+        { name = "modern_industrialization:aluminum_plate",     count = 1 },
+        { name = "modern_industrialization:basic_machine_hull", count = 1 },
+        { name = "modern_industrialization:aluminum_plate",     count = 1 },
+        { name = "modern_industrialization:aluminum_plate",     count = 1 },
+        { name = "modern_industrialization:aluminum_plate",     count = 1 },
+        { name = "modern_industrialization:aluminum_plate",     count = 1 }
+    },
+    outputs = {
+        { name = "modern_industrialization:advanced_machine_casing", count = 1 }
+    }
+}
---
+++ apps/autocraft_v2/recipes/aluminum_machines/frostproof_machine_casing.lua
@@ -0,0 +1,17 @@
+return {
+    machine = "crafter",
+    inputs = {
+        { name = "modern_industrialization:aluminum_plate", count = 1 },
+        { name = "modern_industrialization:aluminum_plate", count = 1 },
+        { name = "modern_industrialization:aluminum_plate", count = 1 },
+        { name = "modern_industrialization:aluminum_plate", count = 1 },
+        { name = "modern_industrialization:aluminum_gear",  count = 1 },
+        { name = "modern_industrialization:aluminum_plate", count = 1 },
+        { name = "modern_industrialization:aluminum_plate", count = 1 },
+        { name = "modern_industrialization:aluminum_plate", count = 1 },
+        { name = "modern_industrialization:aluminum_plate", count = 1 }
+    },
+    outputs = {
+        { name = "modern_industrialization:frostproof_machine_casing", count = 1 }
+    }
+}
