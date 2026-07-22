e669c6277fd2b6e7106ab1fee0aa56ec2a98b13a
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-22 19:57:37 +0200

    feat: add advanced machine hull recipe

---
+++ apps/autocraft_v2/recipes/aluminum_machines/advanced_machine_hull.lua
@@ -0,0 +1,15 @@
+return {
+    machine = "crafter",
+    inputs = {
+        [2] = { name = "modern_industrialization:electronic_circuit",      count = 1 },
+        [4] = { name = "modern_industrialization:silicon_battery",         count = 1 },
+        [5] = { name = "modern_industrialization:advanced_machine_casing", count = 1 },
+        [6] = { name = "modern_industrialization:silicon_battery",         count = 1 },
+        [7] = { name = "modern_industrialization:electrum_cable",          count = 1 },
+        [8] = { name = "modern_industrialization:electrum_cable",          count = 1 },
+        [9] = { name = "modern_industrialization:electrum_cable",          count = 1 }
+    },
+    outputs = {
+        { name = "modern_industrialization:advanced_machine_hull", count = 1 }
+    }
+}
