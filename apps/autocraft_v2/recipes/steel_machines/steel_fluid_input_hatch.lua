9326fad0d63c57cdbfdd8a1912ec2ff5af73e585
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-22 12:14:48 +0200

    feat: add steel hatches recipes

---
+++ apps/autocraft_v2/recipes/steel_machines/steel_fluid_input_hatch.lua
@@ -0,0 +1,10 @@
+return {
+    machine = "crafter",
+    inputs = {
+        [1] = { name = "modern_industrialization:steel_tank",           count = 1 },
+        [4] = { name = "modern_industrialization:steel_machine_casing", count = 1 }
+    },
+    outputs = {
+        { name = "modern_industrialization:steel_fluid_input_hatch", count = 1 }
+    }
+}
---
+++ apps/autocraft_v2/recipes/steel_machines/steel_item_input_hatch.lua
@@ -0,0 +1,10 @@
+return {
+    machine = "crafter",
+    inputs = {
+        [1] = { name = "minecraft:hopper",                              count = 1 },
+        [4] = { name = "modern_industrialization:steel_machine_casing", count = 1 }
+    },
+    outputs = {
+        { name = "modern_industrialization:steel_item_input_hatch", count = 1 }
+    }
+}
