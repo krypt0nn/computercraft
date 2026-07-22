7350b561c7279a389b364b4999647cb4476a7170
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-22 15:03:22 +0200

    fix: fix cupronickel cable recipe again

--- apps/autocraft_v2/recipes/cupronickel_parts/cupronicklel_cable.lua
+++ apps/autocraft_v2/recipes/cupronickel_parts/cupronicklel_cable.lua
@@ -1,15 +1,15 @@
 return {
     machine = "crafter",
     inputs = {
-        { name = "modern_industrialization:rubber_sheet",      count = 1 },
-        { name = "modern_industrialization:rubber_sheet",      count = 1 },
-        { name = "modern_industrialization:rubber_sheet",      count = 1 },
-        { name = "modern_industrialization:cupronicklel_wire", count = 1 },
-        { name = "modern_industrialization:cupronicklel_wire", count = 1 },
-        { name = "modern_industrialization:cupronicklel_wire", count = 1 },
-        { name = "modern_industrialization:rubber_sheet",      count = 1 },
-        { name = "modern_industrialization:rubber_sheet",      count = 1 },
-        { name = "modern_industrialization:rubber_sheet",      count = 1 }
+        { name = "modern_industrialization:rubber_sheet",     count = 1 },
+        { name = "modern_industrialization:rubber_sheet",     count = 1 },
+        { name = "modern_industrialization:rubber_sheet",     count = 1 },
+        { name = "modern_industrialization:cupronickel_wire", count = 1 },
+        { name = "modern_industrialization:cupronickel_wire", count = 1 },
+        { name = "modern_industrialization:cupronickel_wire", count = 1 },
+        { name = "modern_industrialization:rubber_sheet",     count = 1 },
+        { name = "modern_industrialization:rubber_sheet",     count = 1 },
+        { name = "modern_industrialization:rubber_sheet",     count = 1 }
     },
     outputs = {
         { name = "modern_industrialization:cupronickel_cable", count = 3 }
