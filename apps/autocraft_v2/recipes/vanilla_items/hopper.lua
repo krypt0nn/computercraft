b67217ea76a66699e1879fdafd4ab9d196159574
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-22 12:24:26 +0200

    feat: add hopper recipe

---
+++ apps/autocraft_v2/recipes/vanilla_items/hopper.lua
@@ -0,0 +1,14 @@
+return {
+    machine = "crafter",
+    inputs = {
+        [1] = { name = "minecraft:iron_ingot", count = 1 },
+        [3] = { name = "minecraft:iron_ingot", count = 1 },
+        [4] = { name = "minecraft:iron_ingot", count = 1 },
+        [5] = { name = "minecraft:chest",      count = 1 },
+        [6] = { name = "minecraft:iron_ingot", count = 1 },
+        [8] = { name = "minecraft:iron_ingot", count = 1 }
+    },
+    outputs = {
+        { name = "minecraft:hopper", count = 1 }
+    }
+}
