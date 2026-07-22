6271b3a99bf96180f6bd59d95f1292f0d452e1d5
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-22 18:15:35 +0200

    feat: add aluminum tank and barrel recipes

---
+++ apps/autocraft_v2/recipes/aluminum_machines/aluminum_barrel.lua
@@ -0,0 +1,17 @@
+return {
+    machine = "crafter",
+    inputs = {
+        { name = "modern_industrialization:aluminum_plate", count = 1 },
+        { name = "modern_industrialization:aluminum_plate", count = 1 },
+        { name = "modern_industrialization:aluminum_plate", count = 1 },
+        { name = "modern_industrialization:aluminum_plate", count = 1 },
+        { name = "minecraft:barrel",                        count = 1 },
+        { name = "modern_industrialization:aluminum_plate", count = 1 },
+        { name = "modern_industrialization:aluminum_plate", count = 1 },
+        { name = "modern_industrialization:aluminum_plate", count = 1 },
+        { name = "modern_industrialization:aluminum_plate", count = 1 }
+    },
+    outputs = {
+        { name = "modern_industrialization:aluminum_barrel", count = 1 }
+    }
+}
