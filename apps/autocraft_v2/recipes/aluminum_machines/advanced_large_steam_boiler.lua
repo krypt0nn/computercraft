15bba479dc9fd28bfc75074acc6491e4fade6a82
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-23 23:58:23 +0200

    feat: add steam boilers recipes

---
+++ apps/autocraft_v2/recipes/aluminum_machines/advanced_large_steam_boiler.lua
@@ -0,0 +1,17 @@
+return {
+    machine = "crafter",
+    inputs = {
+        { name = "modern_industrialization:pump",               count = 1 },
+        { name = "modern_industrialization:electronic_circuit", count = 1 },
+        { name = "modern_industrialization:pump",               count = 1 },
+        { name = "modern_industrialization:large_pump",         count = 1 },
+        { name = "modern_industrialization:large_steam_boiler", count = 1 },
+        { name = "modern_industrialization:large_pump",         count = 1 },
+        { name = "modern_industrialization:pump",               count = 1 },
+        { name = "modern_industrialization:electronic_circuit", count = 1 },
+        { name = "modern_industrialization:pump",               count = 1 }
+    },
+    outputs = {
+        { name = "modern_industrialization:advanced_large_steam_boiler", count = 1 }
+    }
+}
---
+++ apps/autocraft_v2/recipes/bronze_machines/large_steam_boiler.lua
@@ -0,0 +1,17 @@
+return {
+    machine = "crafter",
+    inputs = {
+        { name = "modern_industrialization:bronze_plated_bricks", count = 1 },
+        { name = "modern_industrialization:analog_circuit",       count = 1 },
+        { name = "modern_industrialization:bronze_plated_bricks", count = 1 },
+        { name = "modern_industrialization:bronze_curved_plate",  count = 1 },
+        { name = "modern_industrialization:bronze_boiler",        count = 1 },
+        { name = "modern_industrialization:bronze_curved_plate",  count = 1 },
+        { name = "modern_industrialization:bronze_plated_bricks", count = 1 },
+        { name = "modern_industrialization:analog_circuit",       count = 1 },
+        { name = "modern_industrialization:bronze_plated_bricks", count = 1 }
+    },
+    outputs = {
+        { name = "modern_industrialization:large_steam_boiler", count = 1 }
+    }
+}
