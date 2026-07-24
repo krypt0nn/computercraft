95439c44a2ebfe9d24cfaab70d59d4c0371637c0
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-24 15:03:35 +0200

    feat: add distillery recipe

---
+++ apps/autocraft_v2/recipes/electric_machines/distillery.lua
@@ -0,0 +1,17 @@
+return {
+    machine = "crafter",
+    inputs = {
+        { name = "minecraft:glass",                             count = 1 },
+        { name = "modern_industrialization:fluid_pipe",         count = 1 },
+        { name = "minecraft:glass",                             count = 1 },
+        { name = "modern_industrialization:large_pump",         count = 1 },
+        { name = "modern_industrialization:basic_machine_hull", count = 1 },
+        { name = "modern_industrialization:large_pump",         count = 1 },
+        { name = "modern_industrialization:electronic_circuit", count = 1 },
+        { name = "modern_industrialization:electrum_cable",     count = 1 },
+        { name = "modern_industrialization:electronic_circuit", count = 1 }
+    },
+    outputs = {
+        { name = "modern_industrialization:distillery", count = 1 }
+    }
+}
