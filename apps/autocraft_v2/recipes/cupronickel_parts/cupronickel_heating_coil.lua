22ca4ac389e4c8e0fe8d4c3a7a01d619efe9794f
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-24 15:07:31 +0200

    feat: add pyrolyse oven recipe

---
+++ apps/autocraft_v2/recipes/cupronickel_parts/cupronickel_heating_coil.lua
@@ -0,0 +1,10 @@
+return {
+    machine = "assembler",
+    inputs = {
+        { name = "modern_industrialization:cupronickel_wire", count = 4 },
+        { name = "modern_industrialization:copper_ring",      count = 1 }
+    },
+    outputs = {
+        { name = "techreborn:cupronickel_heating_coil", count = 1 }
+    }
+}
---
+++ apps/autocraft_v2/recipes/electric_machines/pyrolyse_oven.lua
@@ -0,0 +1,17 @@
+return {
+    machine = "crafter",
+    inputs = {
+        { name = "techreborn:cupronickel_heating_coil",         count = 1 },
+        { name = "modern_industrialization:inductor",           count = 1 },
+        { name = "techreborn:cupronickel_heating_coil",         count = 1 },
+        { name = "modern_industrialization:inductor",           count = 1 },
+        { name = "modern_industrialization:basic_machine_hull", count = 1 },
+        { name = "modern_industrialization:inductor",           count = 1 },
+        { name = "techreborn:cupronickel_heating_coil",         count = 1 },
+        { name = "modern_industrialization:analog_circuit",     count = 1 },
+        { name = "techreborn:cupronickel_heating_coil",         count = 1 }
+    },
+    outputs = {
+        { name = "modern_industrialization:pyrolyse_oven", count = 1 }
+    }
+}
