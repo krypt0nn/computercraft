392045fc07bfff25cf4935801594f9b4cd7e03f6
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-24 13:20:16 +0200

    fix: fix bronze boiler recupe

--- apps/autocraft_v2/recipes/bronze_machines/bronze_boiler.lua
+++ apps/autocraft_v2/recipes/bronze_machines/bronze_boiler.lua
@@ -1,15 +1,15 @@
 return {
     machine = "crafter",
     inputs = {
-        { name = "modern_industrialization:bronze_plate",     count = 1 },
-        { name = "modern_industrialization:bronze_tank",      count = 1 },
-        { name = "modern_industrialization:bronze_plate",     count = 1 },
-        { name = "modern_industrialization:bronze_plate",     count = 1 },
-        { name = "minecraft:furnace",                         count = 1 },
-        { name = "modern_industrialization:bronze_plate",     count = 1 },
-        { name = "modern_industrialization:fire_clay_bricks", count = 1 },
-        { name = "modern_industrialization:fire_clay_bricks", count = 1 },
-        { name = "modern_industrialization:fire_clay_bricks", count = 1 }
+        { name = "modern_industrialization:bronze_plate",          count = 1 },
+        { name = "modern_industrialization:bronze_tank",           count = 1 },
+        { name = "modern_industrialization:bronze_plate",          count = 1 },
+        { name = "modern_industrialization:bronze_plate",          count = 1 },
+        { name = "modern_industrialization:bronze_machine_casing", count = 1 },
+        { name = "modern_industrialization:bronze_plate",          count = 1 },
+        { name = "modern_industrialization:fire_clay_bricks",      count = 1 },
+        { name = "modern_industrialization:fire_clay_bricks",      count = 1 },
+        { name = "modern_industrialization:fire_clay_bricks",      count = 1 }
     },
     outputs = {
         { name = "modern_industrialization:bronze_boiler", count = 1 }
