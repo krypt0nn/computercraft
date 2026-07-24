0261d3b9c531f1132575df8e0edd17a97e955581
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-24 22:06:04 +0200

    feat: add auto crafting table recipe

---
+++ apps/autocraft_v2/recipes/electric_machines/auto_crafting_table.lua
@@ -0,0 +1,12 @@
+return {
+    machine = "assembler",
+    inputs = {
+        { name = "modern_industrialization:analog_circuit", count = 4 },
+        { name = "minecraft:crafting_table",                count = 2 },
+        { name = "modern_industrialization:robot_arm",      count = 2 },
+        { name = "modern_industrialization:motor",          count = 2 }
+    },
+    outputs = {
+        { name = "techreborn:auto_crafting_table", count = 1 }
+    }
+}
---
+++ apps/autocraft_v2/recipes/vanilla_items/crafting_table.lua
@@ -0,0 +1,12 @@
+return {
+    machine = "crafter",
+    inputs = {
+        [1] = { name = "minecraft:spruce_planks", count = 1 },
+        [2] = { name = "minecraft:spruce_planks", count = 1 },
+        [4] = { name = "minecraft:spruce_planks", count = 1 },
+        [5] = { name = "minecraft:spruce_planks", count = 1 }
+    },
+    outputs = {
+        { name = "minecraft:crafting_table", count = 1 }
+    }
+}
