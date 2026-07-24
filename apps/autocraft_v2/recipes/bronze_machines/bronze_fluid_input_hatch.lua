dcacbe0ce944b74975f223fb078404e594754b3d
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-24 13:12:39 +0200

    feat: add bronze hatches recipes

---
+++ apps/autocraft_v2/recipes/bronze_machines/bronze_fluid_input_hatch.lua
@@ -0,0 +1,10 @@
+return {
+    machine = "crafter",
+    inputs = {
+        [1] = { name = "modern_industrialization:bronze_tank",           count = 1 },
+        [4] = { name = "modern_industrialization:bronze_machine_casing", count = 1 }
+    },
+    outputs = {
+        { name = "modern_industrialization:bronze_fluid_input_hatch", count = 1 }
+    }
+}
---
+++ apps/autocraft_v2/recipes/bronze_machines/bronze_item_output_hatch.lua
@@ -0,0 +1,10 @@
+return {
+    machine = "crafter",
+    inputs = {
+        [1] = { name = "modern_industrialization:bronze_machine_casing", count = 1 },
+        [4] = { name = "minecraft:hopper",                               count = 1 }
+    },
+    outputs = {
+        { name = "modern_industrialization:bronze_item_output_hatch", count = 1 }
+    }
+}
