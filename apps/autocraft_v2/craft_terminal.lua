e8546ef964b3d43f889a70895f1ce3e1faf4f033
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-25 14:53:30 +0200

    feat: connect more machines

--- apps/autocraft_v2/craft_terminal.lua
+++ apps/autocraft_v2/craft_terminal.lua
@@ -38,19 +38,27 @@ local MACHINES = {
     },
     cutter = {
         { input = "minecraft:barrel_13", output = "minecraft:barrel_14", slot_usage = 64 },
-        { input = "minecraft:barrel_64", output = "minecraft:barrel_65", slot_usage = 64 }
+        { input = "minecraft:barrel_64", output = "minecraft:barrel_65", slot_usage = 64 },
+        { input = "minecraft:barrel_86", output = "minecraft:barrel_87", slot_usage = 64 },
+        { input = "minecraft:barrel_88", output = "minecraft:barrel_89", slot_usage = 64 }
     },
     wiremill = {
         { input = "minecraft:barrel_56", output = "minecraft:barrel_57", slot_usage = 64 },
-        { input = "minecraft:barrel_58", output = "minecraft:barrel_59", slot_usage = 64 }
+        { input = "minecraft:barrel_58", output = "minecraft:barrel_59", slot_usage = 64 },
+        { input = "minecraft:barrel_90", output = "minecraft:barrel_91", slot_usage = 64 },
+        { input = "minecraft:barrel_92", output = "minecraft:barrel_93", slot_usage = 64 }
     },
     polarizer = {
         { input = "minecraft:barrel_60", output = "minecraft:barrel_61", slot_usage = 64 },
-        { input = "minecraft:barrel_68", output = "minecraft:barrel_69", slot_usage = 64 }
+        { input = "minecraft:barrel_68", output = "minecraft:barrel_69", slot_usage = 64 },
+        { input = "minecraft:barrel_94", output = "minecraft:barrel_95", slot_usage = 64 },
+        { input = "minecraft:barrel_96", output = "minecraft:barrel_97", slot_usage = 64 }
     },
     assembler = {
-        { input = "minecraft:barrel_62", output = "minecraft:barrel_63", slot_usage = 64 },
-        { input = "minecraft:barrel_66", output = "minecraft:barrel_67", slot_usage = 64 }
+        { input = "minecraft:barrel_62",  output = "minecraft:barrel_63",  slot_usage = 64 },
+        { input = "minecraft:barrel_66",  output = "minecraft:barrel_67",  slot_usage = 64 },
+        { input = "minecraft:barrel_98",  output = "minecraft:barrel_99",  slot_usage = 64 },
+        { input = "minecraft:barrel_100", output = "minecraft:barrel_101", slot_usage = 64 }
     },
     extractor = {
         { input = "minecraft:barrel_72", output = "minecraft:barrel_73", slot_usage = 64 },
