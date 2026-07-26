7e943a7af779aee04b8d6a567fa4dda90d2b11c2
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-26 15:55:11 +0200

    feat: add sand and gravel recipes

---
+++ apps/autocraft_v2/recipes/vanilla_items/gravel.lua
@@ -0,0 +1,9 @@
+return {
+    machine = "macerator",
+    inputs = {
+        { name = "minecraft:cobblestone", count = 1 }
+    },
+    outputs = {
+        { name = "minecraft:gravel", count = 1 }
+    }
+}
---
+++ apps/autocraft_v2/recipes/vanilla_items/sand.lua
@@ -0,0 +1,9 @@
+return {
+    machine = "macerator",
+    inputs = {
+        { name = "minecraft:gravel", count = 1 }
+    },
+    outputs = {
+        { name = "minecraft:sand", count = 1 }
+    }
+}
