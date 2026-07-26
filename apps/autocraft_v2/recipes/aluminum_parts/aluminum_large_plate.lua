aa54dc2075289245f76dfddf7ffddee6b67b1b12
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-26 14:51:53 +0200

    feat: add vacuum freezer recipe

---
+++ apps/autocraft_v2/recipes/aluminum_parts/aluminum_large_plate.lua
@@ -0,0 +1,12 @@
+return {
+    machine = "crafter",
+    inputs = {
+        [1] = { name = "modern_industrialization:aluminum_plate", count = 1 },
+        [2] = { name = "modern_industrialization:aluminum_plate", count = 1 },
+        [4] = { name = "modern_industrialization:aluminum_plate", count = 1 },
+        [5] = { name = "modern_industrialization:aluminum_plate", count = 1 }
+    },
+    outputs = {
+        { name = "modern_industrialization:aluminum_large_plate", count = 1 }
+    }
+}
---
+++ apps/autocraft_v2/recipes/electric_machines/vacuum_freezer.lua
@@ -0,0 +1,17 @@
+return {
+    machine = "crafter",
+    inputs = {
+        { name = "modern_industrialization:large_pump",            count = 1 },
+        { name = "modern_industrialization:large_pump",            count = 1 },
+        { name = "modern_industrialization:large_pump",            count = 1 },
+        { name = "modern_industrialization:electronic_circuit",    count = 1 },
+        { name = "modern_industrialization:advanced_machine_hull", count = 1 },
+        { name = "modern_industrialization:electronic_circuit",    count = 1 },
+        { name = "modern_industrialization:aluminum_large_plate",  count = 1 },
+        { name = "modern_industrialization:electronic_circuit",    count = 1 },
+        { name = "modern_industrialization:aluminum_large_plate",  count = 1 }
+    },
+    outputs = {
+        { name = "modern_industrialization:vacuum_freezer", count = 1 }
+    }
+}
