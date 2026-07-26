61393a88d7ccbdf29e37585783f5c44597dcfec3
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-26 15:19:28 +0200

    feat: add annealed copper recipes

---
+++ apps/autocraft_v2/recipes/annealed_copper_parts/annealed_copper_ingot.lua
@@ -0,0 +1,9 @@
+return {
+    machine = "vacuum_freezer",
+    inputs = {
+        { name = "modern_industrialization:annealed_copper_hot_ingot", count = 1 }
+    },
+    outputs = {
+        { name = "modern_industrialization:annealed_copper_ingot", count = 1 }
+    }
+}
