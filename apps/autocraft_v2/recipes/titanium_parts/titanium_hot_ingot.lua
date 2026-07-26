e8140d6ae2e87bb9d6ec438807bc4cdd15e7e4fa
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-26 15:21:03 +0200

    feat: add titanium recipes

---
+++ apps/autocraft_v2/recipes/titanium_parts/titanium_hot_ingot.lua
@@ -0,0 +1,9 @@
+return {
+    machine = "electric_blast_furnace",
+    inputs = {
+        { name = "modern_industrialization:titanium_dust", count = 1 }
+    },
+    outputs = {
+        { name = "modern_industrialization:titanium_hot_ingot", count = 1 }
+    }
+}
---
+++ apps/autocraft_v2/recipes/titanium_parts/titanium_ingot.lua
@@ -0,0 +1,9 @@
+return {
+    machine = "vacuum_freezer",
+    inputs = {
+        { name = "modern_industrialization:titanium_hot_ingot", count = 1 }
+    },
+    outputs = {
+        { name = "modern_industrialization:titanium_ingot", count = 1 }
+    }
+}
