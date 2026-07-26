fc79eec533aad59aa9dd62d86dd6241b9767764f
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-26 15:44:26 +0200

    feat: add laser engraver recipe

---
+++ apps/autocraft_v2/recipes/electric_machines/laser_engraver.lua
@@ -0,0 +1,17 @@
+return {
+    machine = "crafter",
+    inputs = {
+        { name = "modern_industrialization:copper_wire",        count = 1 },
+        { name = "minecraft:glass",                             count = 1 },
+        { name = "modern_industrialization:copper_wire",        count = 1 },
+        { name = "modern_industrialization:motor",              count = 1 },
+        { name = "modern_industrialization:basic_machine_hull", count = 1 },
+        { name = "modern_industrialization:motor",              count = 1 },
+        { name = "modern_industrialization:analog_circuit",     count = 1 },
+        { name = "modern_industrialization:copper_wire",        count = 1 },
+        { name = "modern_industrialization:analog_circuit",     count = 1 }
+    },
+    outputs = {
+        { name = "modern_industrialization:laser_engraver", count = 1 }
+    }
+}
