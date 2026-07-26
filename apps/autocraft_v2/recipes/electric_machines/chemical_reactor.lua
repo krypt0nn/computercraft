caf248ba680deca94a09ab3da6f6b3882fae44e6
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-26 14:49:39 +0200

    feat: add chemical reactor recipe

---
+++ apps/autocraft_v2/recipes/electric_machines/chemical_reactor.lua
@@ -0,0 +1,17 @@
+return {
+    machine = "crafter",
+    inputs = {
+        { name = "minecraft:glass",                             count = 1 },
+        { name = "modern_industrialization:large_pump",         count = 1 },
+        { name = "minecraft:glass",                             count = 1 },
+        { name = "modern_industrialization:aluminum_rotor",     count = 1 },
+        { name = "modern_industrialization:basic_machine_hull", count = 1 },
+        { name = "modern_industrialization:aluminum_rotor",     count = 1 },
+        { name = "modern_industrialization:electronic_circuit", count = 1 },
+        { name = "modern_industrialization:electrum_cable",     count = 1 },
+        { name = "modern_industrialization:electronic_circuit", count = 1 }
+    },
+    outputs = {
+        { name = "modern_industrialization:chemical_reactor", count = 1 }
+    }
+}
