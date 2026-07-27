971198a33ab02fc0d22e3d4d09a4c12a1215cf55
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-27 14:01:48 +0200

    fix: fix me export bus recipe

--- apps/autocraft_v2/recipes/applied_energistics/me_export_bus.lua
+++ apps/autocraft_v2/recipes/applied_energistics/me_export_bus.lua
@@ -1,10 +1,10 @@
 return {
     machine = "crafter",
     inputs = {
+        [1] = { name = "modern_industrialization:steel_plate", count = 1 },
         [2] = { name = "ae2:formation_core",                   count = 1 },
-        [4] = { name = "modern_industrialization:steel_plate", count = 1 },
-        [5] = { name = "minecraft:piston",                     count = 1 },
-        [6] = { name = "modern_industrialization:steel_plate", count = 1 }
+        [3] = { name = "modern_industrialization:steel_plate", count = 1 },
+        [5] = { name = "minecraft:piston",                     count = 1 }
     },
     outputs = {
         { name = "ae2:export_bus", count = 1 }
