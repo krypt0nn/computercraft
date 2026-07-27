18726d3a9322df8d6e57a20d3a1aaba8fc59ccf6
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-27 23:40:27 +0200

    feat: add ME crafting unit recipe

---
+++ apps/autocraft_v2/recipes/applied_energistics/crafting_unit.lua
@@ -0,0 +1,17 @@
+return {
+    machine = "crafter",
+    inputs = {
+        { name = "modern_industrialization:aluminum_plate", count = 1 },
+        { name = "ae2:calculation_processor",               count = 1 },
+        { name = "modern_industrialization:aluminum_plate", count = 1 },
+        { name = "ae2:fluix_glass_cable",                   count = 1 },
+        { name = "ae2:logic_processor",                     count = 1 },
+        { name = "ae2:fluix_glass_cable",                   count = 1 },
+        { name = "modern_industrialization:aluminum_plate", count = 1 },
+        { name = "ae2:calculation_processor",               count = 1 },
+        { name = "modern_industrialization:aluminum_plate", count = 1 }
+    },
+    outputs = {
+        { name = "ae2:crafting_unit", count = 1 }
+    }
+}
