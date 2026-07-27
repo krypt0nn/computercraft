37f1407f7b308b81343f8f6cce398f806b7b72ae
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-27 13:46:32 +0200

    feat: add annihilation plane recipe

---
+++ apps/autocraft_v2/recipes/applied_energistics/me_annihilation_plane.lua
@@ -0,0 +1,14 @@
+return {
+    machine = "crafter",
+    inputs = {
+        { name = "ae2:fluix_crystal",                    count = 1 },
+        { name = "ae2:fluix_crystal",                    count = 1 },
+        { name = "ae2:fluix_crystal",                    count = 1 },
+        { name = "modern_industrialization:steel_plate", count = 1 },
+        { name = "ae2:annihilation_core",                count = 1 },
+        { name = "modern_industrialization:steel_plate", count = 1 }
+    },
+    outputs = {
+        { name = "ae2:annihilation_plane", count = 1 }
+    }
+}
