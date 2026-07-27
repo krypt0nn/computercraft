5cc3efec93d9ad374ee4c814a65ce2eeaa23bea0
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-27 23:13:21 +0200

    feat: add AE2 p2p tunnel recipe

---
+++ apps/autocraft_v2/recipes/applied_energistics/me_p2p_tunnel.lua
@@ -0,0 +1,15 @@
+return {
+    machine = "crafter",
+    inputs = {
+        [2] = { name = "modern_industrialization:steel_plate", count = 1 },
+        [4] = { name = "modern_industrialization:steel_plate", count = 1 },
+        [5] = { name = "ae2:engineering_processor",            count = 1 },
+        [6] = { name = "modern_industrialization:steel_plate", count = 1 },
+        [7] = { name = "ae2:fluix_crystal",                    count = 1 },
+        [8] = { name = "ae2:fluix_crystal",                    count = 1 },
+        [9] = { name = "ae2:fluix_crystal",                    count = 1 }
+    },
+    outputs = {
+        { name = "ae2:me_p2p_tunnel", count = 1 }
+    }
+}
