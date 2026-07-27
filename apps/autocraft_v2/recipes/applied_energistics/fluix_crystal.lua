bf9392da58b66c7f3d958a39cbb99f05f70a667a
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-27 13:03:04 +0200

    fix: fix fluix crystal recipe

--- apps/autocraft_v2/recipes/applied_energistics/fluix_crystal.lua
+++ apps/autocraft_v2/recipes/applied_energistics/fluix_crystal.lua
@@ -1,7 +1,7 @@
 return {
     machine = "mixer",
     inputs = {
-        { name = "minecraft:nether_quartz",           count = 1 },
+        { name = "minecraft:quartz",                  count = 1 },
         { name = "ae2:charged_certus_quartz_crystal", count = 1 },
         { name = "minecraft:redstone",                count = 1 }
     },
