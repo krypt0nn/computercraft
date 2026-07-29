3bff418fa355a93f8668f4e4306ff84b8a074b20
Nikita Podvirnyi <krypt0nn@dawn.wine>
2026-07-29 13:34:20 +0200

    fix: fix some AE2 recipes

--- apps/autocraft_v2/recipes/applied_energistics/blank_pattern.lua
+++ apps/autocraft_v2/recipes/applied_energistics/blank_pattern.lua
@@ -2,11 +2,11 @@ return {
     machine = "crafter",
     inputs = {
         { name = "ae2:quartz_glass",                        count = 1 },
-        { name = "minecraft:glowstone",                     count = 1 },
+        { name = "minecraft:glowstone_dust",                count = 1 },
         { name = "ae2:quartz_glass",                        count = 1 },
-        { name = "minecraft:glowstone",                     count = 1 },
+        { name = "minecraft:glowstone_dust",                count = 1 },
         { name = "ae2:certus_quartz_crystal",               count = 1 },
-        { name = "minecraft:glowstone",                     count = 1 },
+        { name = "minecraft:glowstone_dust",                count = 1 },
         { name = "modern_industrialization:aluminum_plate", count = 1 },
         { name = "modern_industrialization:aluminum_plate", count = 1 },
         { name = "modern_industrialization:aluminum_plate", count = 1 }
---
+++ apps/autocraft_v2/recipes/applied_energistics/crafting_accelerator.lua
@@ -0,0 +1,10 @@
+return {
+    machine = "crafter",
+    inputs = {
+        { name = "ae2:engineering_processor", count = 1 },
+        { name = "ae2:crafting_unit",         count = 1 }
+    },
+    outputs = {
+        { name = "ae2:crafting_accelerator", count = 1 }
+    }
+}
